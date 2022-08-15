71148d06ebbf89c74bf8f9586a2147b36a37c623
OK.Ami <37844281+Nuhe@users.noreply.github.com>
2022-08-15 20:00:13 -0300

    Actualizando mi biblioteca de Powershell

---
+++ CreadorCarpetaYArchivos.ps1
@@ -0,0 +1,21 @@
+﻿function Crear{
+    try{
+        $name = Read-Host "Nombre de la carpeta"
+        $file = Read-Host "¿Quiere crear un archivo en el destino?"
+        $fileName = Read-Host "Nombre del archivo"
+        mkdir -path c:\ -name $name -ErrorAction stop
+        
+        if ($file -match "si"){
+            
+            New-Item -path c:\$name -name $fileName
+        }
+    }
+    catch{
+        Write-Warning $Error[0]
+    }
+}
+
+Write-Host "###Creador de carpetas y archivos###"
+
+Crear
+
---
+++ Global.ps1
@@ -0,0 +1,21 @@
+﻿
+function Get-Global {
+$list = "Descubriste un secreto. ¡¡Felicidades!!", "C:\Users\ext_crodrigu\Downloads\ScriptsPowershell\ping-t.ps1", "C:\Users\ext_crodrigu\Downloads\ScriptsPowershell\NetUser.ps1", "\\pfsnas01\PESA\PESA_CSER_TICO_OTEL\Reservada\Atencion Al Cliente\soporte tecnico y 555\Soporte de Campo\Software\Calculadora\calculadora para W10\qalculate-3.11.0-i386.msi", "C:\Users\ext_crodrigu\Downloads\ScriptsPowershell\desbloquearusuario.ps1", "C:\Users\ext_crodrigu\Downloads\ScriptsPowershell\abrirad.ps1", "C:\Users\ext_crodrigu\Downloads\ScriptsPowershell\ResetPass.ps1" , "C:\Users\ext_crodrigu\Downloads\ScriptsPowershell\CmRcViewer.ps1"
+$opcion = read-host("
+1- Ping -t 
+2- NetUser
+3- Calculadora
+4- Desbloquear Usuario (BETA)
+5- Abrir AD (BETA)
+6- Reiniciar Contraseña (BETA)
+7- RemoteControl (BETA)
+
+Elige el Script")
+
+start $list[$opcion]
+}
+
+do {
+    Get-Global
+    $Continue = read-host("¿Desea utilizar otro Script? s/n")
+} while ($Continue -eq "s")
